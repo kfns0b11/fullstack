6a98699b1315e81dead3d41e8f9eb61aad1c2054
kfngp <kfnsjc@gmail.com>
2021-12-28 21:12:56 +0800

    exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -18,31 +18,49 @@ const Header = ({ content }) => {
   )
 }
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
   const all = good + neutral + bad
   const average = (good - bad) / all
   const positive = `${(good / all) * 100} %`
 
+  if (all !== 0) {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
+    )
+  }
+
   return (
     <>
-    <p>
-      good {good}<br />
-      neutral {neutral}<br />
-      bad {bad}<br />
-      all {all}<br />
-      average {average}<br />
-      positive {positive}<br />
-    </p>
-  </>
+      <p>No feedback given</p>
+    </>
   )
+
 
 }
 
+const StatisticLine = ({ text, value }) => (
+  <>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  </>
+)
+
 const App = () => {
   // save clicks of each button to own state
-  const [good, setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   const handleGoodClick = () => setGood(good + 1)
   const handleNeutralClick = () => setNeutral(neutral + 1)
