c4558294f66c332a04ec8034dab001da50021c30
kfngp <kfnsjc@gmail.com>
2021-12-29 09:36:07 +0800

    exercise 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -8,6 +8,40 @@ const Button = ({ handleClick, text }) => (
   </>
 )
 
+const MostVotes = ({ anecdotes, votes }) => {
+  console.log(anecdotes, votes)
+  const highestCount = Math.max(...votes)
+
+  const indexs = []
+  let index = votes.indexOf(highestCount)
+  while (index !== -1) {
+    indexs.push(index)
+    index = votes.indexOf(highestCount, index+1)
+  }
+
+  console.log("highestCount", highestCount, "indexs", indexs, "index", index)
+  if (highestCount === 0) {
+    return (
+      <>
+        <p>No votes yet</p>
+      </>
+    )
+  }
+
+  return (
+    <>
+      {
+        indexs.map(item => (
+          <p>
+            {anecdotes[item]}
+          </p>
+        ))
+      }
+      <p>has {highestCount} votes</p>
+    </>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -18,18 +52,38 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-   
+
   const [selected, setSelected] = useState(0)
 
   const anecdotesLen = anecdotes.length
-  
-  const handleRandomClick = () => setSelected(getRandomInt(anecdotesLen))
+  const [votes, setVotes] = useState(Array(anecdotesLen).fill(0))
+
+
+
   const getRandomInt = (max) => Math.floor(Math.random() * max)
+
+  const handleRandomClick = () => {
+    let random = getRandomInt(anecdotesLen)
+    while (random === selected) {
+      random = getRandomInt
+    }
+    setSelected(random)
+  }
+  const handleVotesClick = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={handleVotesClick} text="vote" />
       <Button handleClick={handleRandomClick} text="next anecdote" />
+      <h1>Anecdote with most votes</h1>
+      <MostVotes anecdotes={anecdotes} votes={votes} />
     </div>
   )
 }
